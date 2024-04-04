11e94fc3ef92e244d84a62f3edf5bb03badff3ef
JM_ARAUJO <jmaz.dev@gmail.com>
2024-04-04 15:57:53 -0300

    first commit and finish app

---
+++ app/src/main/java/com/numbersort/appsortnumber/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.numbersort.appsortnumber
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ScrollingView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sortear(view: View) {
+
+        val textResult = findViewById<TextView>(R.id.txtResult)
+        val number = Random().nextInt(11)
+
+        textResult.text = "Resultado: $number"
+    }
+
+}
